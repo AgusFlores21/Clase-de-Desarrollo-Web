7a5c9d1f699588ecc0672a77708b6db2af86efeb
AgusFlores <agustinfloresfranco@gmail.com>
2023-05-22 13:48:50 -0300

    First comit

---
+++ index.js
@@ -0,0 +1,35 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  const currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (currentPosition > 0) {
+    window.requestAnimationFrame(topFunction);
+    window.scrollTo(0, currentPosition - currentPosition / 8);
+  }
+}
+
+const images = document.querySelectorAll('.gallery-img');
+const modal = document.querySelector('.modal');
+const modalImg = document.querySelector('.modal-img');
+
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    modalImg.src = image.src;
+    modal.style.display = 'block';
+  });
+});
+
+modal.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+
