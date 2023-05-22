e2a6b369e4c6a38704febc367baa7ca87e3687d6
AgusFlores <agustinfloresfranco@gmail.com>
2023-05-22 16:51:32 -0300

    Fixed

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-window.onscroll = function() {scrollFunction()};
+document.addEventListener('DOMContentLoaded', function() {
+  window.onscroll = function() {
+    scrollFunction();
+  };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
   }
-}
 
-function topFunction() {
-  const currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  function topFunction() {
+    const currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
-  if (currentPosition > 0) {
-    window.requestAnimationFrame(topFunction);
-    window.scrollTo(0, currentPosition - currentPosition / 8);
+    if (currentPosition > 0) {
+      window.requestAnimationFrame(topFunction);
+      window.scrollTo(0, currentPosition - currentPosition / 8);
+    }
   }
-}
 
-const images = document.querySelectorAll('.gallery-img');
-const modal = document.querySelector('.modal');
-const modalImg = document.querySelector('.modal-img');
+  const images = document.querySelectorAll('.gallery-img');
+  const modal = document.querySelector('.modal');
+  const modalImg = document.querySelector('.modal-img');
 
-images.forEach(image => {
-  image.addEventListener('click', () => {
-    modalImg.src = image.src;
-    modal.style.display = 'block';
+  images.forEach(image => {
+    image.addEventListener('click', () => {
+      modalImg.src = image.src;
+      modal.style.display = 'block';
+    });
+
+    image.addEventListener('touchstart', () => {
+      modalImg.src = image.src;
+      modal.style.display = 'block';
+    });
+  });
+
+  modal.addEventListener('click', () => {
+    modal.style.display = 'none';
   });
 });
-
-modal.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-
